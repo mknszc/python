e98da04888151f2c7c68e671996201f6ba78de77
Özcan ÇAMOĞLU <ozcan.camoglu@bil.omu.edu.tr>
2018-07-30 11:23:39 +0300

    .

--- harfsay.p.py
+++ harfsay.p.py
@@ -1,11 +1,11 @@
-class HarfSayacı:
+class HarfSayaci:
     def __init__(self):
         self.sesli_harfler = 'aeıioöuüAEIİOÖUÜ'
         self.sessiz_harfler = 'bcçdfgğhjklmnprsştvyzBCÇDFGĞHJKLMNPRSŞTVYZ'
         self.bosluk_karekteri = ' '
-        self.sayaç_sesli = 0
-        self.sayaç_sessiz = 0
-        self.sayaç_bosluk = 0
+        self.sayac_sesli = 0
+        self.sayac_sessiz = 0
+        self.sayac_bosluk = 0
         
     def kelime_sor(self):
         return input('Bir kelime veya cümle girin: ')
@@ -19,25 +19,25 @@ class HarfSayacı:
     def bosluktur(self, harf):
         return harf in self.bosluk_karekteri
     
-    def artır(self):
+    def artir(self):
         for harf in self.kelime:
             if self.seslidir(harf):
-                self.sayaç_sesli += 1
+                self.sayac_sesli += 1
             elif self.sessizdir(harf):
-                self.sayaç_sessiz += 1
+                self.sayac_sessiz += 1
             elif self.bosluktur(harf):
-                self.sayaç_bosluk += 1
-        return (self.sayaç_sesli, self.sayaç_sessiz, self.sayaç_bosluk)
+                self.sayac_bosluk += 1
+        return (self.sayac_sesli, self.sayac_sessiz, self.sayac_bosluk)
                 
     def ekrana_bas(self):
         sesli, sessiz, bosluk = self.artır()
         mesaj = "{} kelimesinde {} sesli {} sessiz harf ve {} bosluk var."
         print(mesaj.format(self.kelime, sesli, sessiz, bosluk))
         
-    def çalıştır(self):
+    def calistir(self):
         self.kelime = self.kelime_sor()
         self.ekrana_bas()
 
 if __name__ == '__main__':
     sayaç = HarfSayacı()
-    sayaç.çalıştır() 
+    sayaç.calistir() 
