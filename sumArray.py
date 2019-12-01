aa91dcfcab745de8de619c6589abab1656c4ca09
ITUSER <ddd>
2019-12-01 22:40:44 +0300

    check value type

--- sumArray.py
+++ sumArray.py
@@ -1,5 +1,9 @@
 def sumArray(array = []):
     sumArray = 0
     for i in array:
-        sumArray += int(i)
+        if type(i) == int :
+            sumArray += int(i)
     print(sumArray)
+
+if __name__ == '__main__':
+    sumArray([1, 2, 3, 4, 'a'])
