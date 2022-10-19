0faffb1f4aabf79b80e58930bd053b572721e6d8
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2022-10-19 17:23:44 +0300

    Delete deneme.py

--- deneme.py
+++
@@ -1,9 +0,0 @@
-class Çalışan():
-    kabiliyetleri = ['sınıf niteliği']
-    def __init__(self):
-        self.kabiliyetleri = ['örnek niteliği']
-#sınıf niteliğine erişmek için #sınıf adını kullanıyoruz
-print(Çalışan.kabiliyetleri)
-#örnek niteliğine erişmek için #örnek adını kullanıyoruz
-ahmet = Çalışan()
-print(ahmet.kabiliyetleri) 
